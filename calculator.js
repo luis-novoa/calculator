7c61501578519866c9c9f40187a7e5b46166e018
Luis Novoa <luis.matte.novoa@gmail.com>
2019-08-14 12:42:38 -0400

    Four basic operations set up

---
+++ calculator.js
@@ -0,0 +1,15 @@
+function add (num1, num2) {
+    return num1 + num2;
+};
+
+function substract (num1, num2) {
+    return num1 - num2;
+};
+
+function multiply (num1, num2) {
+    return num1 * num2;
+};
+
+function divide (num1, num2) {
+    return num1 / num2;
+};
