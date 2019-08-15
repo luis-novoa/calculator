379e3a4ec8717e5ccea2b1b252f34c5e1537189f
Luis Novoa <luis.matte.novoa@gmail.com>
2019-08-15 10:50:06 -0400

    Dividing by zero is impossible now

--- calculator.js
+++ calculator.js
@@ -11,7 +11,12 @@ function multiply (num1, num2) {
 };
 
 function divide (num1, num2) {
-    return num1 / num2;
+    if (num2 == 0){
+        alert('Dividing by zero is still impossible :(');
+        return 0;
+    } else {
+        return num1 / num2;
+    }
 };
 
 let pastEquals = 0;
