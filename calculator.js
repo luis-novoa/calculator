adc3a01bfcb0f7db17fcfd1d273e6924f642f941
Luis Novoa <luis.matte.novoa@gmail.com>
2019-08-14 21:43:13 -0400

    Numbers are printed on the screen and stored

--- calculator.js
+++ calculator.js
@@ -27,3 +27,23 @@ for (let i = 0; i < 12; i++) {
     numBtn.innerHTML = numpad.numbers[i];
     num.appendChild(numBtn)
 };
+
+let buttons = document.querySelectorAll('button');
+for (let i = 0; i < buttons.length; i++) {
+    let numTest = Number(buttons[i].textContent);
+    if (numTest >= 0 || buttons[i].textContent == '.') {
+        buttons[i].addEventListener('click', () => {
+            screenPlay(buttons[i].textContent)
+        });
+    }
+}
+
+let screen = document.querySelector('.screen');
+let screenNumber = document.querySelector('p');
+let number = [];
+function screenPlay(value) {
+    if (screenNumber == 0) { screenNumber.textContent = ''};
+    screenNumber.textContent += value.toString();
+    screen.appendChild(screenNumber);
+    number.push(value);
+}
